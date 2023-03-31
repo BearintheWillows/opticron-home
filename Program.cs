a877467f7f2dc51dd9e62b5680401b39edabcdd0
Stuart <47339755+BearintheWillows@users.noreply.github.com>
2023-03-31 10:55:55 +0100

    Added ContentService.cs to handle database funcs. Added to Program.cs for Dependency Injection

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using optricon_home;
 using optricon_home.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,7 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<AppDbContext>( opt => opt.UseSqlServer( builder.Configuration.GetConnectionString( "DefaultConnection" ) ) );
 
+builder.Services.AddScoped<IContentService, ContentService>();
 
 var app = builder.Build();
 
